d889c70775b528c4eb6398e831ff4f31d058a1cb
showermax <sevenhauz@gmail.com>
2023-05-09 16:35:52 +0300

    lesson 20 - useState+useMemo

--- src/Component/Optimization Tests/Usestate.stories.tsx
+++ src/Component/Optimization Tests/Usestate.stories.tsx
@@ -1,11 +1,18 @@
-import {useRef, useState} from "react";
+import {useMemo, useRef, useState} from "react";
 
 export default {
     title: 'Demo Usestate',
 }
 
+function VeryHeavyCalc (){
+    let k=0
+    while(k<1000000000) k++
+    return 10
+}
 export const UseStateTest = () => {
-    const [c,setC]=useState(0)
+    const init = useMemo(VeryHeavyCalc,[])
+    const [c,setC]=useState(init)
+
     return <>
 
         <button onClick={()=>setC(c+1)}> increase </button>
